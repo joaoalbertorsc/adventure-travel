0bba06b2678cf68b3e8ee5889fc12a29ef5f042d
joaoalbertorsc <jaoalbertorsc@gmail.com>
2024-02-08 21:14:33 -0300

    one more time

--- src/main/java/com/adventuretravel/plants/controllers/TravelAdventuresController.java
+++ src/main/java/com/adventuretravel/plants/controllers/TravelAdventuresController.java
@@ -1,71 +1,42 @@
 package com.adventuretravel.plants.controllers;
 
 import com.adventuretravel.plants.entities.Adventure;
-import com.adventuretravel.plants.repositories.AdventureRepository;
+import com.adventuretravel.plants.service.AdventureService;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.http.HttpStatus;
+
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/traveladventures")
 public class TravelAdventuresController {
 
-    private final AdventureRepository adventureRepository;
-
-    public TravelAdventuresController(AdventureRepository adventureRepo) {
-        this.adventureRepository = adventureRepo;
+    private final AdventureService adventureService;
+    public TravelAdventuresController(AdventureService adventureServ) {
+        this.adventureService = adventureServ;
     }
-
     @GetMapping()
     public Iterable<Adventure> getAllAdventures(){
-        Iterable<Adventure> adventures = adventureRepository.findAll();
-        return adventures;
+        return adventureService.getAllAdventures();
     }
     @GetMapping("/bycountry/{country}")
     public List<Adventure> getAdventuresByCountry(@PathVariable ("country") String country){
-        List<Adventure> adventuresByCountry = new ArrayList<>();
-        for(Adventure adventure: adventureRepository.findAll()){
-            if(adventure.getCountry().equals(country)){
-                adventuresByCountry.add(adventure);
-            }
-        }
-        return adventuresByCountry;
+        return adventureService.getAdventureByCountry(country);
     }
 
     @GetMapping("/traveladventures/bystate")
     public List<Adventure> getAdventureByState(@RequestParam(name = "state") String state){
-        List<Adventure> adventuresByState = new ArrayList<>();
-        for(Adventure adventure: adventureRepository.findAll()){
-            if(adventure.getState().equals(state)){
-                adventuresByState.add(adventure);
-            }
-        }
-        return adventuresByState;
+        return adventureService.getAdventureByState(state);
     }
     @PostMapping("/traveladventures")
     public void saveAdventure(@RequestBody Adventure adventure){
-        adventureRepository.save(adventure);
+        adventureService.saveAdventure(adventure);
     }
     @PutMapping("/traveladventures/{id}")
     public Adventure getAdventureById(@PathVariable("id") int id, @RequestBody Adventure adventure){
-        Optional<Adventure> adventureOptional = this.adventureRepository.findById(id);
-        if(!adventureOptional.isPresent()){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Adventure not found with id: " + id);
-        }
-        Adventure adv = adventureOptional.get();
-        adv.setBlogCompleted(true);
-        return adv;
+        return adventureService.getAdventureById(id);
     }
     @DeleteMapping("/traveladventures/{id}")
     public void deleteAdventureById(@PathVariable("id") int id){
-        Optional<Adventure> adventureOptional = this.adventureRepository.findById(id);
-        if(!adventureOptional.isPresent()){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Adventure not found with id: " + id);
-        }
-        Adventure adv = adventureOptional.get();
-        adventureRepository.delete(adv);
+        adventureService.deleteAdventureById(id);
     }
 }
--- src/main/java/com/adventuretravel/plants/repositories/AdventureRepository.java
+++ src/main/java/com/adventuretravel/plants/repositories/AdventureRepository.java
@@ -3,9 +3,26 @@ package com.adventuretravel.plants.repositories;
 import com.adventuretravel.plants.entities.Adventure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface AdventureRepository extends CrudRepository<Adventure, Integer> {
-    public List<Adventure> findByCountry(String country);
-    public List<Adventure> findByState(String state);
+    public default List<Adventure> findByCountry(String country){
+        List<Adventure> adventuresByCountry = new ArrayList<>();
+        for(Adventure adventure: this.findAll()){
+            if(adventure.getCountry().equals(country)){
+                adventuresByCountry.add(adventure);
+            }
+        }
+        return adventuresByCountry;
+    }
+    public default List<Adventure> findByState(String state){
+        List<Adventure> adventuresByState = new ArrayList<>();
+        for (Adventure adventure: this.findAll()){
+            if (adventure.getState().equals(state)){
+                adventuresByState.add(adventure);
+            }
+        }
+        return adventuresByState;
+    }
 }
