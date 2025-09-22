0ddf8f542bb0775089d04124069f90faec76f508
joaoalbertorsc <jaoalbertorsc@gmail.com>
2025-09-22 13:43:49 -0300

    chore: Rename PlantsApplication to AdventureTravelApplication

--- src/main/java/com/adventuretravel/plants/AdventureTravelApplication.java
+++ src/main/java/com/adventuretravel/plants/AdventureTravelApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PlantsApplication {
+public class AdventureTravelApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PlantsApplication.class, args);
+		SpringApplication.run(AdventureTravelApplication.class, args);
 	}
 
 }
